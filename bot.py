97fa509c5c2e9d37b179ca3331cf00ae46688b79
Valeria Chemtai <valeriachemtai28@gmail.com>
2020-03-13 15:41:55 +0300

    add qna maker (#3)

--- bot.py
+++ bot.py
@@ -1,12 +1,29 @@
+from botbuilder.ai.qna import QnAMaker, QnAMakerEndpoint
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
 
+from flask import Config
+
 
 class AfricaCapitalBot(ActivityHandler):
-    async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(
-            'Africa Nations Capitals bot services will be available soon.'
+    def __init__(self, config: Config):
+        # instantiate qnamaker
+        self.qna_maker = QnAMaker(
+            QnAMakerEndpoint(
+                knowledge_base_id=config.QNA_KNOWLEDGEBASE_ID,
+                endpoint_key=config.QNA_ENDPOINT_KEY,
+                host=config.QNA_ENDPOINT_HOST,
+            )
         )
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        # make call to qnamaker service
+        response = await self.qna_maker.get_answers(turn_context)
+        if response and len(response) > 0:
+            await turn_context.send_activity(MessageFactory.text(
+                f'The Capital of {response[0].questions[0]} is {response[0].answer}'))
+        else:
+            await turn_context.send_activity(f'The Capital for the country {turn_context.activity.text} was not found.')
 
     async def on_members_added_activity(
         self,
--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 
 class DefaultConfig(object):
-    APP_ID = os.getenv("MicrosoftAppId", "")
-    APP_PASSWORD = os.getenv("MicrosoftAppPassword", "")
+    APP_ID = os.getenv('MicrosoftAppId', '')
+    APP_PASSWORD = os.getenv('MicrosoftAppPassword', '')
     PORT = os.getenv('PORT', '5000')
     HOST = os.getenv('HOST', 'localhost')
+
+    QNA_KNOWLEDGEBASE_ID = os.environ.get('QNA_KNOWLEDGEBASE_ID', '')
+    QNA_ENDPOINT_KEY = os.environ.get('QNA_ENDPOINT_KEY', '')
+    QNA_ENDPOINT_HOST = os.environ.get('QNA_ENDPOINT_HOST', '')
