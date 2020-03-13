bc5965226b7d0dd98c1222e409c916151007fad1
valeria-chemtai <valeriachemtai28@gmail.com>
2020-03-13 13:12:02 +0300

    add message handler

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 
 class DefaultConfig(object):
+    APP_ID = os.getenv("MicrosoftAppId", "")
+    APP_PASSWORD = os.getenv("MicrosoftAppPassword", "")
     PORT = os.getenv('PORT', '5000')
     HOST = os.getenv('HOST', 'localhost')
